d6242bf2e166766a04bf1d5676c5703a5ef1e0f5
Sahil <ss6611786@gmail.com>
2023-08-14 19:30:26 +0530

    so for using nativenavigation we need to install one more package navigation-stack after that i have created two files first is home.js and second is profile.js

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Home />
+      <Profile />
       <StatusBar style="auto" />
     </View>
   );
